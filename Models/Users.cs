f2947351d00cb7e7b81b83c5ed270a87ed31ad8d
Jordan <jis.iza@gmail.com>
2024-08-13 12:30:46 -0500

    update method get

--- Models/Users.cs
+++ Models/Users.cs
@@ -3,7 +3,7 @@ namespace MyApi.Models
     public class User
     {
         public required int Id { get; set; }
-        public required string Name { get; set; }
+        public required string Name {get; set; }
         public required string Password { get; set; }
     }
 }
